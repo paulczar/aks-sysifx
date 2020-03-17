fc479cbe5b2611ad9cfc150c72305f5dadc8abf7
Jesse Noller <jnoller@gmail.com>
2020-03-17 11:28:18 -0600

    checkpoint for dash loading

--- monitoring/envs.sh
+++ monitoring/envs.sh
@@ -6,4 +6,5 @@ export ENV_NAME=monitoring
 export ENV_DIR=".."
 
 ## Grafana
-export GRAFANA_ADMIN_PASSWORD=io-debug
+# Change this for your install please
+export GRAFANA_ADMIN_PASSWORD=I2vx7nLlIES0ZUWc5czysA6M4xatXc21hzCDkqed
